0d715bf5caa03935663ed5cd0ab1a47f9ea5dc80
Levi Sørum <levi@kraftlauget.no>
2024-01-05 13:38:13 +0100

    Restrict sanity create button to english version

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,7 +6,7 @@ import { schema } from './sanity/schema';
 import { documentInternationalization } from '@sanity/document-internationalization';
 import structure from './sanity/desk/structure';
 import { translatedTypes, singletonTypes } from './sanity/desk/definition';
-import { languageNames, languages } from './types/language';
+import { defaultLanguage, languageNames, languages } from './types/language';
 
 const singletonActions = new Set(['publish', 'discardChanges', 'restore']);
 
@@ -18,7 +18,23 @@ export default defineConfig({
 		...schema,
 
 		templates: (templates) =>
-			templates.filter(({ schemaType }) => !singletonTypes.has(schemaType)),
+			templates
+				.filter(({ schemaType }) => !singletonTypes.has(schemaType))
+				.filter(({ id }) => !translatedTypes.includes(id))
+				.filter(({ id }) => {
+					for (let i = 0; i < languages.length; i++) {
+						const language = languages[i];
+						for (let k = 0; k < translatedTypes.length; k++) {
+							const schemaType = translatedTypes[k];
+							if (
+								language !== defaultLanguage &&
+								id === `${schemaType}-${language}`
+							)
+								return false;
+						}
+					}
+					return true;
+				}),
 	},
 	plugins: [
 		deskTool({
@@ -34,13 +50,22 @@ export default defineConfig({
 				};
 			}),
 			schemaTypes: translatedTypes,
+			apiVersion,
 		}),
 	],
 	document: {
-		actions: (input, context) =>
-			singletonTypes.has(context.schemaType)
+		actions: (input, context) => {
+			return singletonTypes.has(context.schemaType)
 				? input.filter(({ action }) => action && singletonActions.has(action))
-				: input,
+				: input;
+		},
+		newDocumentOptions: (prev, { creationContext }) => {
+			const { type } = creationContext;
+			if (type === 'global') {
+				return [];
+			}
+			return prev;
+		},
 	},
 	redirects: () => {
 		return [
