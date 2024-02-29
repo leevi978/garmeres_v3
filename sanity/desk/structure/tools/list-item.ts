1077b76eb5fa507c6e0ecf013b5c952e38c564be
Levi Sorum <levsor1@Levi-sin-MacBook-Pro.local>
2024-02-29 08:33:49 +0100

    Sanity studio sort blog posts by createdAT

--- sanity/desk/structure/tools/list-item.ts
+++ sanity/desk/structure/tools/list-item.ts
@@ -3,40 +3,45 @@ import createViews, { ViewOptions } from './views';
 import { apiVersion } from '@/sanity/env';
 
 export type ListItemOptions = {
-	title?: string;
-	singleton?: boolean;
+    title?: string;
+    singleton?: boolean;
 } & ViewOptions;
 
 function createFilter(schemaType: string, translated?: boolean) {
-	return `_type == "${schemaType}"${translated ? ` && language != "se"` : ''}`;
+    return `_type == "${schemaType}"${
+        translated ? ` && language != "se"` : ''
+    }`;
 }
 
 export default function createListItem(
-	S: StructureBuilder,
-	schemaType: string,
-	options?: ListItemOptions
+    S: StructureBuilder,
+    schemaType: string,
+    options?: ListItemOptions
 ) {
-	const { title, singleton, ...viewOptions } = options || {};
-	return S.listItem()
-		.id(schemaType)
-		.title(title || schemaType)
-		.child(
-			singleton
-				? S.document()
-						.id(schemaType)
-						.schemaType(schemaType)
-						.documentId(schemaType)
-						.views(createViews(S, schemaType, viewOptions))
-				: S.documentList()
-						.id(schemaType)
-						.schemaType(schemaType)
-						.apiVersion(apiVersion)
-						.filter(createFilter(schemaType, viewOptions.translated))
-						.child((documentId) =>
-							S.document()
-								.id(documentId)
-								.schemaType(schemaType)
-								.views(createViews(S, schemaType, viewOptions))
-						)
-		);
+    const { title, singleton, ...viewOptions } = options || {};
+    return S.listItem()
+        .id(schemaType)
+        .title(title || schemaType)
+        .child(
+            singleton
+                ? S.document()
+                      .id(schemaType)
+                      .schemaType(schemaType)
+                      .documentId(schemaType)
+                      .views(createViews(S, schemaType, viewOptions))
+                : S.documentList()
+                      .id(schemaType)
+                      .schemaType(schemaType)
+                      .apiVersion(apiVersion)
+                      .filter(createFilter(schemaType, viewOptions.translated))
+                      .defaultOrdering([
+                          { field: '_createdAt', direction: 'desc' },
+                      ])
+                      .child((documentId) =>
+                          S.document()
+                              .id(documentId)
+                              .schemaType(schemaType)
+                              .views(createViews(S, schemaType, viewOptions))
+                      )
+        );
 }
