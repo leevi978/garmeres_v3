6a8eed5756505c444e066f32536882a715fadb6e
Levi Sorum <levsor1@Levi-sin-MacBook-Pro.local>
2024-05-07 16:58:23 +0200

    Fix GA cookie delete

--- app/components/analytics/analytics-interactive.tsx
+++ app/components/analytics/analytics-interactive.tsx
@@ -1,13 +1,15 @@
 "use client";
 import { GoogleAnalytics } from "@next/third-parties/google";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Language } from "@/types/language";
 import CookieBanner from "./cookie-banner";
 import { setCookie } from "cookies-next";
+import { deleteGACookies } from "@/app/actions/ga-cookies";
 
 type AnalyticsProps = {
   language: Language;
   initialConsent?: boolean;
+  initialDeleteCookies?: boolean;
   consentCookieName: string;
 };
 
@@ -16,11 +18,17 @@ export default function AnalyticsInteractive(props: AnalyticsProps) {
     props.initialConsent
   );
 
+  useEffect(() => {
+    if (props.initialDeleteCookies === true)
+      deleteGACookies(props.consentCookieName);
+  }, [props.initialDeleteCookies]);
+
   function onResponse(response: boolean) {
     setCookie(props.consentCookieName, response ? "true" : "false", {
       maxAge: response ? 28 * 24 * 60 * 60 : undefined, // 28 days, in seconds
     });
     setConsent(response);
+    if (!response) deleteGACookies(props.consentCookieName);
   }
 
   return consent === true ? (
--- app/components/analytics/index.tsx
+++ app/components/analytics/index.tsx
@@ -1,28 +1,20 @@
+"use server";
 import { Language } from "@/types/language";
-import { cookies } from "next/headers";
 import AnalyticsInteractive from "./analytics-interactive";
+import { getGAConsent, hasGACookies } from "@/app/actions/ga-cookies";
 
 const consentCookieName = "_ga_consent";
 
 export default async function Analytics({ language }: { language: Language }) {
-  const cookieStore = cookies();
-  const cookieValue = cookieStore.get(consentCookieName)?.value;
-  const consent = cookieValue ? cookieValue === "true" : undefined;
-
-  if (consent !== true) {
-    // Delete all GA cookies
-    cookieStore
-      .getAll()
-      .map((c) => c.name)
-      .filter((c) => c.startsWith("_ga") && c !== consentCookieName)
-      .forEach((c) => cookieStore.delete(c));
-  }
-
+  const initialConsent = await getGAConsent(consentCookieName);
   return (
     <AnalyticsInteractive
       consentCookieName={consentCookieName}
       language={language}
-      initialConsent={consent}
+      initialConsent={initialConsent}
+      initialDeleteCookies={
+        !initialConsent && (await hasGACookies(consentCookieName))
+      }
     />
   );
 }
